cb013cb1de48b595020235136590ef847adfcd4e
Michael Bevilacqua <bev@akuls-air.bc.edu>
2023-10-10 22:15:03 -0400

    Final

---
+++ BIOl_Mod_5.bib.R
@@ -0,0 +1,12 @@
+@article{gleeson1998,
+  title={Temperature regulation during exercise},
+  author={M Gleeson},
+  year={1998}
+}
+
+
+@article{michael1993,
+  title={Physiological Responses to Exercise in the Heat},
+  author={Michael N. Sawka,1 C. Bruce Wenger, Andrew J. Young, and Kent B. Pandolf},
+  year={1993}
+}
